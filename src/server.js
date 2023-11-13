442950886f95a1a3f4717f5a12e5ddd8dd6e7bbd
Mamoru Matsunaga <pineb1ue10ngtree@gmail.com>
2023-11-13 11:43:46 +0900

    feat: Implement and test GET /api/characters

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const hpData = require('./data/characters.json')
 
 const setupServer = () => {
     const app = express()
     app.use(express.json())
     app.use(express.text())
+
+    /** GET /api/characters */
+    app.get('/characters', (req, res) => {
+        if (req.query.limit) {
+            res.json({ result: hpData.characters.slice(0, req.query.limit) })
+        } else {
+            res.json({ result: hpData.characters })
+        }
+    })
 
     return app
 }
--- test/_harryPotterApi.js
+++ test/_harryPotterApi.js
@@ -1,6 +1,8 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
+
 const { setupServer } = require('../src/server')
+const hpData = require('../src/data/characters.json')
 
 chai.use(chaiHttp)
 chai.should()
@@ -10,5 +12,19 @@ describe('Harry Potter API Server', () => {
     let request
     beforeEach(() => {
         request = chai.request(server)
+    })
+
+    describe('GET /characters - returning harry potter characters', () => {
+        it('should return harry potter characters', async () => {
+            const res = await request.get('/characters')
+            const expected = hpData.characters
+            JSON.parse(res.text).result.should.deep.equal(expected)
+        })
+
+        it('should return harry potter characters', async () => {
+            const res = await request.get('/characters').query({ limit: 5 })
+            const expected = hpData.characters
+            JSON.parse(res.text).result.should.deep.equal(expected.slice(0, 5))
+        })
     })
 })
